2b4a8a009bfdf79b0eb372e70df91c8a43a7f94d
Matt Nelson-White <mnelsonwhite@gmail.com>
2020-09-02 11:22:15 +1000

    Add IEnumerableExtension to get range

---
+++ src/Range.Net/IEnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Range.Net.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Range.Net
+{
+    public static class IEnumerableExtensions
+    {
+        public static IRange<TProperty> Range<TValue, TProperty>(
+            this IEnumerable<TValue> enumerable,
+            Func<TValue, TProperty> propertySelector,
+            RangeInclusivity inclusivity = RangeInclusivity.InclusiveMinInclusiveMax) where TProperty : IComparable<TProperty>
+        {
+            var minmax = enumerable.Aggregate(
+                new MutablePair<TProperty>(),
+                (agg, val) => {
+                    var propertyValue = propertySelector(val);
+                    agg.Value1 = propertyValue.CompareTo(agg.Value1) < 0 ? propertyValue : agg.Value1;
+                    agg.Value2 = propertyValue.CompareTo(agg.Value2) > 0 ? propertyValue : agg.Value2;
+                    return agg;
+                }
+            );
+            return new Range<TProperty>(
+                minmax.Value1,
+                minmax.Value2,
+                inclusivity
+            );
+        }
+
+        private class MutablePair<T>
+        {
+            public T Value1 { get; set; }
+            public T Value2 { get; set; }
+        }
+    }
+}
